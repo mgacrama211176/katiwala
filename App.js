ce61237e652aea18058404395e5620d7bc3ea70c
marlonG <mgacrama_ccs@uspf.edu.ph>
2023-11-09 19:24:52 +0800

    Add gradient background and loading bar to Home screen

--- App.js
+++ App.js
@@ -23,7 +23,6 @@ export default function App() {
             component={HomeScreen}
             options={{
               headerShown: false,
-              transitionSpec: {},
             }}
           />
           <Stack.Screen name="Login" component={Login} />
--- screens/Home/index.jsx
+++ screens/Home/index.jsx
@@ -1,10 +1,52 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Pressable } from "react-native";
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <Text>HOME</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <LinearGradient
+        colors={["#06C09F", "#1EB52D", "#06C07D"]}
+        style={{ flex: 1 }}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              width: 650,
+              height: 650,
+              borderRadius: 700 / 2,
+              backgroundColor: "white",
+              alignItems: "center",
+              justifyContent: "center",
+              borderWidth: 1,
+              borderColor: "white",
+            }}
+          >
+            <Text style={{ fontSize: 24 }}>KATIWALA</Text>
+            {/* Add a loading Bar */}
+            <Text
+              style={{
+                fontSize: 24,
+                borderWidth: 1,
+                width: "50%",
+                borderRadius: 10,
+                textAlign: "center",
+              }}
+            >
+              70%
+            </Text>
+            <Pressable>
+              <Text>BUtton</Text>
+            </Pressable>
+          </View>
+        </View>
+      </LinearGradient>
     </SafeAreaView>
   );
 };
