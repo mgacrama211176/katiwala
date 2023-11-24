fc270db5e15705184c579469085a8ad2793f5bff
marlonG <mgacrama_ccs@uspf.edu.ph>
2023-11-24 18:22:03 +0800

    Add camera plugin and update dependencies

--- screens/Register/FaceVerify.jsx
+++ screens/Register/FaceVerify.jsx
@@ -1,12 +1,64 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { Camera, CameraType } from "expo-camera";
 
 const FaceVerify = () => {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, setPermission] = Camera.useCameraPermissions();
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+  setPermission(true);
+
+  if (permission.status !== "granted") {
+    return (
+      <View>
+        <Text>Permission not granted</Text>
+      </View>
+    );
+  }
+
   return (
     <View>
       <Text>FaceVerify</Text>
+      <View style={styles.container}>
+        <Camera type={type}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+});
 
 export default FaceVerify;
