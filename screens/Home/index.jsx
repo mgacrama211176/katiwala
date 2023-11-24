da4c4d441ec4b5225b17ab3018f8242f4f225e58
marlon osia <osiamarlon101@gmail.com>
2023-11-24 18:06:32 +0800

    uncommet 7 to 12

--- screens/Home/index.jsx
+++ screens/Home/index.jsx
@@ -1,13 +1,15 @@
 import { View, SafeAreaView, Image } from "react-native";
 import React, { useEffect } from "react";
 import logo from "../../assets/logo.png";
+import { Button } from "react-native";
+
 const Home = ({ navigation }) => {
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     //navigate to login screen
-  //     navigation.navigate("Login");
-  //   }, 3000);
-  // }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      //navigate to login screen
+      navigation.navigate("Login");
+    }, 3000);
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -20,7 +22,7 @@ const Home = ({ navigation }) => {
       >
         {/* Image */}
         <View style={{ width: 400, height: 400, }}>
-          <Image source={logo} style={{ width: "100%", height: "100%" }} />
+          <Image source={logo} style={{ width: "100%", height: "100%" }} />      
         </View>
       </View>
     </SafeAreaView>
