7a77f74b4cf988df807e649ca626262fd35afe50
marlon osia <osiamarlon101@gmail.com>
2023-11-24 13:57:34 +0800

    logo.png

--- screens/Home/index.jsx
+++ screens/Home/index.jsx
@@ -1,14 +1,12 @@
-import { View, Text, SafeAreaView, Pressable } from "react-native";
+import { View,  SafeAreaView, Image } from "react-native";
 import React from "react";
-import { LinearGradient } from "expo-linear-gradient";
+import logo from "../../assets/logo.png";
 
 const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <LinearGradient
-        colors={["#06C09F", "#1EB52D", "#06C07D"]}
-        style={{ flex: 1 }}
-      >
+      
+      
         <View
           style={{
             flex: 1,
@@ -27,26 +25,22 @@ const Home = () => {
               borderWidth: 1,
               borderColor: "white",
             }}
-          >
-            <Text style={{ fontSize: 24 }}>KATIWALA</Text>
-            {/* Add a loading Bar */}
-            <Text
-              style={{
-                fontSize: 24,
-                borderWidth: 1,
-                width: "50%",
-                borderRadius: 10,
-                textAlign: "center",
-              }}
-            >
-              70%
-            </Text>
-            <Pressable>
-              <Text>BUtton</Text>
-            </Pressable>
+          >  {/* Image */}
+          <View >
+            <Image source={logo} style={{width:"100%",height:"100%"}} />
+          </View>
+          
+       
+             
+            
+              
+           
+           
+             
+            
           </View>
         </View>
-      </LinearGradient>
+      
     </SafeAreaView>
   );
 };
