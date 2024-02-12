1954e7078ebc670ede9dd790779ceb8236067918
marlonG <mgacrama_ccs@uspf.edu.ph>
2024-02-12 17:55:54 +0800

    updated with dismiss Keyboard function

--- screens/Register/RateGenerator.jsx
+++ screens/Register/RateGenerator.jsx
@@ -1,38 +1,49 @@
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import React from "react";
 import EstimateCard from "../Global components/EstimateCard";
 
 const RateGenerator = () => {
+  const handleDismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
   return (
-    <SafeAreaView
-      style={{
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginTop: 60,
-        marginBottom: 10,
-      }}
-    >
-      <Text style={{ fontSize: 24, fontFamily: "RobotoSlab_400Regular" }}>
-        Estimated Rates
-      </Text>
-      <Text
+    <TouchableWithoutFeedback onPress={handleDismissKeyboard}>
+      <SafeAreaView
         style={{
-          color: "red",
-          fontWeight: "bold",
-          fontSize: 16,
-          width: "80%",
-          textAlign: "center",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 60,
+          marginBottom: 10,
         }}
       >
-        Please be reminded that there will be a 10% service charge per service
-        made.
-      </Text>
+        <Text style={{ fontSize: 24, fontFamily: "RobotoSlab_400Regular" }}>
+          Estimated Rates
+        </Text>
+        <Text
+          style={{
+            color: "red",
+            fontWeight: "bold",
+            fontSize: 16,
+            width: "80%",
+            textAlign: "center",
+          }}
+        >
+          Please be reminded that there will be a 10% service charge per service
+          made.
+        </Text>
 
-      {/* Cards will be inserted here which will depend on the service selected prior */}
-      <EstimateCard />
-    </SafeAreaView>
+        {/* Cards will be inserted here which will depend on the service selected prior */}
+        <EstimateCard />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
